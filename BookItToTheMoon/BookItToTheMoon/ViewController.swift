0fbee0bd828453edf802e7f5672337dd3e2c979c
Tom Stoermer <tomstoermer@googlemail.com>
2016-04-22 17:01:19 +0200

    clean commit

---
+++ BookItToTheMoon/BookItToTheMoon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookItToTheMoon
+//
+//  Created by Tom Störmer on 22.04.16.
+//  Copyright © 2016 SpaceAppsChallenge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
