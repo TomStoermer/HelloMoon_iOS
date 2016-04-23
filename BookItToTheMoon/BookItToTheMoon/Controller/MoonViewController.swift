774b0e3a8441b6100da3033359c2ecc5eb32f108
Tom Stoermer <tomstoermer@googlemail.com>
2016-04-23 15:14:39 +0200

    [ADD] Moon Detail

---
+++ BookItToTheMoon/BookItToTheMoon/Controller/MoonViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MoonViewController.swift
+//  BookItToTheMoon
+//
+//  Created by Tom Störmer on 23.04.16.
+//  Copyright © 2016 SpaceAppsChallenge. All rights reserved.
+//
+
+import UIKit
+
+class MoonViewController: PlanetViewController {
+
+    
+    @IBOutlet weak var planetImageView: UIImageView!
+    @IBOutlet weak var planetTitleLabel: UILabel!
+    @IBOutlet weak var planetDescriptionLabel: UILabel!
+
+    let moon: Moon = Moon(moonPhase: .FullMoon, moonDistance: 375_000.0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // setup imageView
+        planetImageView.image = moon.planetImage
+        planetImageView.alpha = 0.0
+        planetImageView.contentMode = .ScaleAspectFit
+        
+        // setup labels
+        planetTitleLabel.text = moon.planetTitle
+        planetTitleLabel.alpha = 0.0
+        planetDescriptionLabel.text = moon.planetDescription
+        planetDescriptionLabel.alpha = 0.0
+    
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // fade in planet image
+        UIView.animateWithDuration(1.0, delay: 0.0, options: [.CurveEaseInOut], animations: {
+            // fade in planet
+            self.planetImageView.alpha = 1.0
+            }, completion: nil)
+        
+        // fade in title, description
+        UIView.animateWithDuration(0.5, delay: 1.5, options: [.CurveEaseInOut], animations: {
+            // fade in planet
+            self.planetTitleLabel.alpha = 1.0
+            self.planetDescriptionLabel.alpha = 0.8
+            }, completion: nil)
+        
+        // fade in details and other actions
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+
+// MARK: - IBAction
+
+extension MoonViewController {
+    
+    @IBAction func closeButtonPressed(sender: UIButton) {
+        
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+}
