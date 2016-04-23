729a71ab6035afcf4953b3f0a8a1592242e57118
Tom Stoermer <tomstoermer@googlemail.com>
2016-04-23 19:27:14 +0200

    pause commit

--- BookItToTheMoon/BookItToTheMoon/View/PlanetFacts/PlanetFactReasonCell.swift
+++ BookItToTheMoon/BookItToTheMoon/View/PlanetFacts/PlanetFactReasonCell.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class PlanetFactReasonCell: UICollectionViewCell {
 
+    @IBOutlet private weak var factReasonLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        // cell
+        backgroundColor = UIColor.clearColor()
+        contentView.backgroundColor = UIColor.clearColor()
+        
+        // label
+        factReasonLabel.textColor = UIColor.whiteColor()
     }
-
+    
+    
+    func configureFactReason(factReason: String) {
+        factReasonLabel.text = factReason
+    }
 }
